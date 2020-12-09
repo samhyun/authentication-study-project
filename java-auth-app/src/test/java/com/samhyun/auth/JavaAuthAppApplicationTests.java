bae21b210f6f9a9a1b3ea267f6f47dcfd2b4b776
samhyun <rnflal31@gmail.com>
2020-12-09 16:16:17 +0900

    initialize

---
+++ java-auth-app/src/test/java/com/samhyun/auth/JavaAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhyun.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAuthAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
